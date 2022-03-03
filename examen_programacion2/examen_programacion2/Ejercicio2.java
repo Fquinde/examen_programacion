e09e630310ed9fc2dac4b5049f03600c7fa667cb
Fquinde <95882367+Fquinde@users.noreply.github.com>
2022-03-03 18:08:15 +0100

    Add files via upload

---
+++ examen_programacion2/examen_programacion2/Ejercicio2.java
@@ -0,0 +1,38 @@
+package examen_programacion2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+	public static void main (String[]args) {
+		Scanner  entrada= new Scanner(System.in);
+		System.out.println("Introduzca un numero entero: ");
+		int numero=entrada.nextInt();
+		palabrasMasLargasQue(numero);
+		
+	}
+	public static void palabrasMasLargasQue(int longitud) {
+		String fichero1="C:/Users/quind/Downloads/textoExamen.txt";
+		File file = new File (fichero1);
+		int contador = 0;
+		try {
+			Scanner f = new Scanner(file);
+			while(f.hasNextLine()) {
+				String linea = f.nextLine();
+				linea = linea.replaceAll(",","").replaceAll(".", "");
+				String[] lineaSep = linea.split(" ");
+				System.out.println(linea);
+				for(int i=0; i<lineaSep.length; i++) {
+					if(lineaSep[i].length() >=longitud) {
+						contador++;
+						System.out.println(lineaSep[i]);
+					}
+				}
+			}
+			System.out.println("Palabras encontradas:" + contador);
+		}catch(FileNotFoundException e){
+		}
+	}
+
+}
